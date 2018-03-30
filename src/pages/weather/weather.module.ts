8e0dbf0d954b70a01d61bdc84cc65a8552896d8b
Daniel Horowitz <danielhorowitz@zipcoders-MacBook-Pro-80.local>
2018-03-30 09:19:03 -0400

    ready for branching

--- src/pages/weather/weather.module.ts
+++ src/pages/weather/weather.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { WeatherPage } from './weather';
+import { TabsPage } from '../tabs/tabs';
 
 @NgModule({
   declarations: [
     WeatherPage,
+    TabsPage
   ],
   imports: [
     IonicPageModule.forChild(WeatherPage),
